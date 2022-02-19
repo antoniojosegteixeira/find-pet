a46821e66dcd4da8f180bf1e70c67846fd59280c
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-19 05:47:35 -0400

    fix: show correct routes

--- src/routes/PrivateRoutes.js
+++ src/routes/PrivateRoutes.js
@@ -1,16 +1,14 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import Home from "../pages/Home";
 import Profile from "../pages/Profile";
 
 export default function PrivateRoutes() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/perfil" element={<Profile />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/perfil" element={<Profile />}></Route>
+    </Routes>
   );
 }
--- src/routes/PublicRoutes.js
+++ src/routes/PublicRoutes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import Login from "../pages/Login";
 import Register from "../pages/Register";
@@ -7,12 +7,10 @@ import Home from "../pages/Home";
 
 export default function PublicRoutes() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/registrar" element={<Register />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/registrar" element={<Register />}></Route>
+    </Routes>
   );
 }
