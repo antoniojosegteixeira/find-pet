47624cf8c5bf7d6de29cd58f9540178f6e7f1196
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-19 03:08:04 -0400

    feat: add auth functions

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCJsVAkv-zbisi7-wyRYWVK0Eg_rIo9AVI",
@@ -9,5 +11,9 @@ const firebaseConfig = {
   appId: "1:365015473209:web:ff4ed3e3e878ff5fb581e8",
 };
 
-// Initialize Firebase
+// Init Firebase
 initializeApp(firebaseConfig);
+
+// Init Services
+export const auth = getAuth();
+export const database = getFirestore();
