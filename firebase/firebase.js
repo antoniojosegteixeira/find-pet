f76d58e25eea333a206c0acbf80fb9c98e1ad14b
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-19 03:05:08 -0400

    feat: add firebase

---
+++ firebase/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJsVAkv-zbisi7-wyRYWVK0Eg_rIo9AVI",
+  authDomain: "petfinder-9a284.firebaseapp.com",
+  projectId: "petfinder-9a284",
+  storageBucket: "petfinder-9a284.appspot.com",
+  messagingSenderId: "365015473209",
+  appId: "1:365015473209:web:ff4ed3e3e878ff5fb581e8",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
