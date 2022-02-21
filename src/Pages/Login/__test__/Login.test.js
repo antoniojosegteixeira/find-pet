4004fb202726099eb54df37b739e5d79d49a21ce
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-21 08:10:33 -0400

    test: fix error tests

--- src/Pages/Login/__test__/Login.test.js
+++ src/Pages/Login/__test__/Login.test.js
@@ -3,6 +3,7 @@ import "@testing-library/jest-dom";
 import store from "../../../Redux/Store/store";
 import faker from "@faker-js/faker";
 import { Provider } from "react-redux";
+import { loginErrMgs } from "../../../Validation/Schemas/login-schema";
 
 import Login from "../Login";
 
@@ -33,15 +34,21 @@ describe("Login page", () => {
     expect(loginEmailInput.value).toBe(emailText);
   });
 
-  test("should show error on incorrect email", () => {
+  test("should show error on incorrect email", async () => {
     render(<LoginComponent />);
     const incorrectEmailText = "random";
     const loginEmailInput = screen.getByTestId("login-email-input");
-    const emailError = screen.getByTestId("login-email-error");
     fireEvent.change(loginEmailInput, {
       target: { value: incorrectEmailText },
     });
-    expect(emailError.value).not.toBe("");
+
+    // Submitting
+    const submitButton = screen.getByTestId("login-submit-button");
+    fireEvent.click(submitButton);
+
+    expect(
+      await screen.findByText(loginErrMgs.invalidEmail)
+    ).toBeInTheDocument();
   });
 
   test("should have password input", () => {
@@ -58,25 +65,43 @@ describe("Login page", () => {
     expect(loginPasswordInput.value).toBe(passwordText);
   });
 
-  test("should show error on short password", () => {
+  test("should show error on short password", async () => {
     render(<LoginComponent />);
+
+    // Filling the password field with incorrect info
     const shortPasswordText = "123";
     const loginPasswordInput = screen.getByTestId("login-password-input");
-    const passwordError = screen.getByTestId("login-password-error");
     fireEvent.change(loginPasswordInput, {
       target: { value: shortPasswordText },
     });
-    expect(passwordError.value).not.toBe("");
+
+    // Submitting
+    const submitButton = screen.getByTestId("login-submit-button");
+    fireEvent.click(submitButton);
+
+    // Getting error field
+    expect(
+      await screen.findByText(loginErrMgs.minPassword)
+    ).toBeInTheDocument();
   });
 
-  test("should show error on long password", () => {
+  test("should show error on long password", async () => {
     render(<LoginComponent />);
-    const longPasswordText = new Array(40 + 1).join("#");
+
+    // Filling password with 50 characters
+    const longPasswordText = new Array(50).join("#");
     const loginPasswordInput = screen.getByTestId("login-password-input");
-    const passwordError = screen.getByTestId("login-password-error");
     fireEvent.change(loginPasswordInput, {
       target: { value: longPasswordText },
     });
-    expect(passwordError.value).not.toBe("");
+
+    // Submitting
+    const submitButton = screen.getByTestId("login-submit-button");
+    fireEvent.click(submitButton);
+
+    // Getting error field
+    expect(
+      await screen.findByText(loginErrMgs.maxPassword)
+    ).toBeInTheDocument();
   });
 });
