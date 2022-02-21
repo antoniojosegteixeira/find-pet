0d3eac2038f0a73ede3a26bf076c93eaa76b3229
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-21 06:07:19 -0400

    feat: add registration validation schema

---
+++ src/Validation/Schemas/registration-schema.js
@@ -0,0 +1,19 @@
+import * as yup from "yup";
+
+const registrationSchema = yup.object().shape({
+  name: yup
+    .string("Campo inválido")
+    .min(2, "Nome deve conter no mínimo 2 caracteres")
+    .required("Campo obrigatório"),
+  email: yup
+    .string("Campo inválido")
+    .email("Insira um email válido")
+    .required("Campo obrigatório"),
+  password: yup
+    .string("Campo inválido")
+    .min(6, "Senha deve conter no mínimo 6 dígitos")
+    .max(40, "Senha deve conter no máximo 40 dígitos")
+    .required("Campo obrigatório"),
+});
+
+export default registrationSchema;
