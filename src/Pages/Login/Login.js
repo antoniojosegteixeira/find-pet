4432716048672b1be1c77528d43d1a2ea1ac2f28
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-21 04:41:28 -0400

    feat: add login page

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,26 +1,48 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { login } from "../../Redux/Slices/userSlice";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import loginSchema from "../../Validation/Schemas/login-schema";
 
 export default function Login() {
   const dispatch = useDispatch();
-  const onSubmit = (e) => {
-    e.preventDefault();
-    dispatch(
-      login({
-        name: "João",
-        email: "joaodasdas@gmail.com",
-        password: "648dsad323",
-      })
-    );
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(loginSchema),
+  });
+
+  const onSubmit = (data) => {
+    // Dispatch as action to redux
+    dispatch(login(data));
   };
 
   return (
     <div data-testid="login-wrapper">
       Login
-      <input data-testid="login-email-input" label="email"></input>
-      <input data-testid="login-password-input" label="password"></input>
-      <button onSubmit={onSubmit}>Enviar</button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          data-testid="login-email-input"
+          name="email"
+          {...register("email")}
+        ></input>
+        <br />
+        <span data-testid="login-email-error">{errors.email?.message}</span>
+        <input
+          data-testid="login-password-input"
+          name="password"
+          type="password"
+          {...register("password")}
+        ></input>
+        <span data-testid="login-password-error">
+          {errors.password?.message}
+        </span>
+        <button onSubmit={onSubmit}>Enviar</button>
+      </form>
     </div>
   );
 }
