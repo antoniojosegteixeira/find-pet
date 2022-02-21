43b2c777050c9c64df56c355d3f96242c325d246
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-21 02:09:42 -0400

    feat: add dispatch function to login

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { login } from "../../Redux/Slices/userSlice";
 
 export default function Login() {
+  const dispatch = useDispatch();
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch(
+      login({
+        name: "João",
+        email: "joaodasdas@gmail.com",
+        password: "648dsad323",
+      })
+    );
+  };
+
   return (
     <div data-testid="login-wrapper">
       Login
       <input data-testid="login-email-input" label="email"></input>
       <input data-testid="login-password-input" label="password"></input>
+      <button onSubmit={onSubmit}>Enviar</button>
     </div>
   );
 }
