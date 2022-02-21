e36d560caec6914887ec7548648430524c3fb0ae
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-21 04:40:38 -0400

    test: add login input tests

--- src/Pages/Login/__test__/Login.test.js
+++ src/Pages/Login/__test__/Login.test.js
@@ -1,6 +1,7 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import store from "../../../Redux/Store/store";
+import faker from "@faker-js/faker";
 import { Provider } from "react-redux";
 
 import Login from "../Login";
@@ -26,9 +27,21 @@ describe("Login page", () => {
 
   test("should write text on email input", () => {
     render(<LoginComponent />);
+    const emailText = faker.internet.email();
     const loginEmailInput = screen.getByTestId("login-email-input");
-    fireEvent.change(loginEmailInput, { target: { value: "any@mail.com" } });
-    expect(loginEmailInput.value).toBe("any@mail.com");
+    fireEvent.change(loginEmailInput, { target: { value: emailText } });
+    expect(loginEmailInput.value).toBe(emailText);
+  });
+
+  test("should show error on incorrect email", () => {
+    render(<LoginComponent />);
+    const incorrectEmailText = "random";
+    const loginEmailInput = screen.getByTestId("login-email-input");
+    const emailError = screen.getByTestId("login-email-error");
+    fireEvent.change(loginEmailInput, {
+      target: { value: incorrectEmailText },
+    });
+    expect(emailError.value).not.toBe("");
   });
 
   test("should have password input", () => {
@@ -39,8 +52,31 @@ describe("Login page", () => {
 
   test("should write text on password input", () => {
     render(<LoginComponent />);
+    const passwordText = faker.internet.password();
     const loginPasswordInput = screen.getByTestId("login-password-input");
-    fireEvent.change(loginPasswordInput, { target: { value: "11d$%891dsa8" } });
-    expect(loginPasswordInput.value).toBe("11d$%891dsa8");
+    fireEvent.change(loginPasswordInput, { target: { value: passwordText } });
+    expect(loginPasswordInput.value).toBe(passwordText);
+  });
+
+  test("should show error on short password", () => {
+    render(<LoginComponent />);
+    const shortPasswordText = "123";
+    const loginEmailInput = screen.getByTestId("login-email-input");
+    const passwordError = screen.getByTestId("login-email-error");
+    fireEvent.change(loginEmailInput, {
+      target: { value: shortPasswordText },
+    });
+    expect(passwordError.value).not.toBe("");
+  });
+
+  test("should show error on long password", () => {
+    render(<LoginComponent />);
+    const longPasswordText = new Array(40 + 1).join("#");
+    const loginEmailInput = screen.getByTestId("login-email-input");
+    const passwordError = screen.getByTestId("login-email-error");
+    fireEvent.change(loginEmailInput, {
+      target: { value: longPasswordText },
+    });
+    expect(passwordError.value).not.toBe("");
   });
 });
