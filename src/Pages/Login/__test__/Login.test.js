615e6f5b130f28cacfb547259bfc438bfffe6f1d
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-21 04:25:22 -0400

    test: add redux provider to login component

--- src/Pages/Login/__test__/Login.test.js
+++ src/Pages/Login/__test__/Login.test.js
@@ -1,36 +1,44 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import store from "../../../Redux/Store/store";
+import { Provider } from "react-redux";
 
 import Login from "../Login";
 
 describe("Login page", () => {
+  const LoginComponent = () => (
+    <Provider store={store}>
+      <Login />
+    </Provider>
+  );
+
   test("should render component", () => {
-    render(<Login />);
+    render(<LoginComponent />);
     const loginWrapper = screen.getByTestId("login-wrapper");
     expect(loginWrapper).toBeInTheDocument();
   });
 
   test("should have email input", () => {
-    render(<Login />);
+    render(<LoginComponent />);
     const loginEmailInput = screen.getByTestId("login-email-input");
     expect(loginEmailInput).toBeInTheDocument();
   });
 
   test("should write text on email input", () => {
-    render(<Login />);
+    render(<LoginComponent />);
     const loginEmailInput = screen.getByTestId("login-email-input");
     fireEvent.change(loginEmailInput, { target: { value: "any@mail.com" } });
     expect(loginEmailInput.value).toBe("any@mail.com");
   });
 
   test("should have password input", () => {
-    render(<Login />);
+    render(<LoginComponent />);
     const loginPasswordInput = screen.getByTestId("login-password-input");
     expect(loginPasswordInput).toBeInTheDocument();
   });
 
   test("should write text on password input", () => {
-    render(<Login />);
+    render(<LoginComponent />);
     const loginPasswordInput = screen.getByTestId("login-password-input");
     fireEvent.change(loginPasswordInput, { target: { value: "11d$%891dsa8" } });
     expect(loginPasswordInput.value).toBe("11d$%891dsa8");
