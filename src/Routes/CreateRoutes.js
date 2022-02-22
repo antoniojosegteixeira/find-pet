f571e642c7a1ddc2d8b894c9f3834cf8f62627cb
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-22 07:00:25 -0400

    feat: add create post route

--- src/Routes/CreateRoutes.js
+++ src/Routes/CreateRoutes.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "../Pages/Login/Login";
 import Register from "../Pages/Register/Register";
+import CreatePost from "../Pages/CreatePost/CreatePost";
 
 const CreateRoutes = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/registrar" element={<Register />} />
+      <Route path="/criar-post" element={<CreatePost />} />
     </Routes>
   </BrowserRouter>
 );
