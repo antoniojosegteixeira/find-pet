a60cb6115a3e03717eed1b1ff44eda626733db5c
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-22 03:38:51 -0400

    feat: add login api request

--- src/Redux/Slices/User/requests.js
+++ src/Redux/Slices/User/requests.js
@@ -1,17 +1,22 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-export const loginUser = createAsyncThunk("auth/loginUser", async () => {
-  const { data } = axios.get("https://jsonplaceholder.typicode.com/users");
-  return data;
-});
+export const loginUser = createAsyncThunk(
+  "auth/loginUser",
+  async (formData) => {
+    const { data } = await axios.post("http://localhost:5000/login", {
+      data: formData,
+    });
+    return data;
+  }
+);
 
 export const extraReducers = {
   [loginUser.pending]: (state) => {
     return { ...state, loginStatus: "loading" };
   },
   [loginUser.fulfilled]: (state, action) => {
-    return { ...state, loginStatus: "success" };
+    return { ...state, loginStatus: "success", user: action.payload };
   },
   [loginUser.rejected]: (state, action) => {
     return { ...state, loginStatus: "failed" };
