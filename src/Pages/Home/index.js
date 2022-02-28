18ae4863c0cdd08824cca93a2be21b46c273a4ec
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-28 09:22:35 -0400

    feat: add home page

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Container, Content, Column } from "./styles";
+
+export default function Home() {
+  return (
+    <Container>
+      <Content></Content>
+      <Content></Content>
+    </Container>
+  );
+}
