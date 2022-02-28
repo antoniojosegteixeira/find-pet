22b4b07eef5bb2fe5b4b892a1f82b4bb35578fdb
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-28 13:46:17 -0400

    feat: add roboto font

--- src/Styles/Theme/theme.js
+++ src/Styles/Theme/theme.js
@@ -2,7 +2,9 @@ import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 
 let theme = createTheme({
   typography: {
-    fontFamily: "Open Sans, Raleway, Arial",
+    p: {
+      fontFamily: "Roboto, Raleway, Arial",
+    },
   },
   palette: {
     primary: {
