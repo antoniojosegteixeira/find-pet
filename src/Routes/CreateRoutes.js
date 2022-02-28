61585720d55096e303bf475c5fffb90579b46a8e
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-28 10:00:39 -0400

    feat: add Home route

--- src/Routes/CreateRoutes.js
+++ src/Routes/CreateRoutes.js
@@ -4,10 +4,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../Pages/Login/Login";
 import Register from "../Pages/Register/Register";
 import CreatePost from "../Pages/CreatePost/CreatePost";
+import Home from "../Pages/Home";
 
 const CreateRoutes = () => (
   <BrowserRouter>
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/registrar" element={<Register />} />
       <Route path="/criar-post" element={<CreatePost />} />
