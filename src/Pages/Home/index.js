59ed9a2934c122e7f266ec8aef8b6badf0c00460
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-28 13:56:47 -0400

    feat(home): add second section

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -2,36 +2,74 @@ import React from "react";
 import styles from "./styles";
 
 import { Container, Grid, Typography, Box, Button } from "@mui/material";
-import { BgSvg } from "./svgs";
+import FirstSectionSvg from "./svg/FirstSectionSvg";
+import SecondSectionSvg from "./svg/SecondSectionSvg";
 import smilingDog from "../../Assets/Images/smiling-dog.png";
+import layingDog from "../../Assets/Images/laying-dog.png";
 
 const Home = () => {
   return (
-    <Container sx={styles.container}>
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={6} lg={6} sx={styles.heroInfo}>
-          <Box sx={styles.heroInfoBox}>
-            <Typography variant="h3" component="h1" sx={styles.title}>
-              FindPet te ajuda a encontrar seu amigo perdido!
-            </Typography>
-            <Button variant="contained" sx={styles.button}>
-              Registrar
-            </Button>
-          </Box>
-        </Grid>
-        <Grid item xs={12} sm={6} sx={styles.heroBackground}>
-          <Box sx={styles.backgroundWrapper}>
-            <Box sx={{ ...styles.animalBubble, ...styles.cat }}></Box>
-            <Box sx={{ ...styles.animalBubble, ...styles.dog }}></Box>
-            <Box sx={{ ...styles.animalBubble, ...styles.iguana }}></Box>
-            <Box sx={{ ...styles.animalBubble, ...styles.bird }}></Box>
+    <>
+      <Container sx={styles.container}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={5} sx={styles.heroInfo}>
+            <Box sx={styles.heroInfoBox}>
+              <Typography variant="h3" component="h1" sx={styles.title}>
+                FindPet te ajuda a encontrar seu amigo perdido!
+              </Typography>
+              <Button variant="contained" sx={styles.button}>
+                Registrar
+              </Button>
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={7} sx={styles.heroBackground}>
+            <Box sx={styles.backgroundWrapper}>
+              <Box sx={{ ...styles.animalBubble, ...styles.cat }}></Box>
+              <Box sx={{ ...styles.animalBubble, ...styles.dog }}></Box>
+              <Box sx={{ ...styles.animalBubble, ...styles.iguana }}></Box>
+              <Box sx={{ ...styles.animalBubble, ...styles.bird }}></Box>
 
-            <img src={smilingDog} alt="Cão de óculos sorrindo" />
-            <BgSvg />
-          </Box>
+              <img src={smilingDog} alt="Cão de óculos sorrindo" />
+              <FirstSectionSvg />
+            </Box>
+          </Grid>
         </Grid>
-      </Grid>
-    </Container>
+      </Container>
+      <Container sx={styles.container}>
+        <Grid container spacing={2}>
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            sx={{ ...styles.heroInfo, ...styles.heroInfoAlt }}
+          >
+            <Box sx={styles.heroInfoBox}>
+              <Typography variant="h3" component="h1" sx={styles.title}>
+                Encontrou algum animal perdido? Publique na FindPet!
+              </Typography>
+              <Typography variant="p" component="p" sx={styles.paragraph}>
+                Milhares de animais são perdidos todos os anos. Ajude donos de
+                pet a reencontrarem seus animais.
+              </Typography>
+              <Button variant="contained" sx={styles.button}>
+                Entrar
+              </Button>
+            </Box>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            sx={{ ...styles.heroBackground, ...styles.heroBackgroundAlt }}
+          >
+            <Box sx={styles.secondBackgroundWrapper}>
+              <img src={layingDog} alt="Cão deitado" />
+              <SecondSectionSvg />
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
   );
 };
 
--- src/Pages/Home/styles.js
+++ src/Pages/Home/styles.js
@@ -4,12 +4,15 @@ import bird from "../../Assets/Images/bird1.jpg";
 import iguana from "../../Assets/Images/iguana1.jpg";
 
 const styles = {
-  container: { border: "1px solid" },
+  container: {},
   heroInfo: {
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
     order: { xs: 2, sm: 1, md: 1, lg: 1 },
+  },
+  heroInfoAlt: {
+    order: { xs: 2, sm: 2, md: 2, lg: 2 },
   },
   heroInfoBox: {
     padding: "0 5px 0 5px",
@@ -24,10 +27,16 @@ const styles = {
       lg: "2.5rem",
     },
   },
+  paragraph: {
+    marginTop: 1,
+  },
   heroBackground: {
     width: { lg: "60%", md: "100%" },
     margin: "0 auto",
     order: { xs: 1, sm: 2, md: 2, lg: 2 },
+  },
+  heroBackgroundAlt: {
+    order: { xs: 1, sm: 1, md: 1, lg: 1 },
   },
   button: {
     marginTop: 4,
@@ -40,6 +49,16 @@ const styles = {
       width: "70%",
       bottom: "5%",
       left: "15%",
+    },
+  },
+  secondBackgroundWrapper: {
+    position: "relative",
+    transform: "translateX(-5%)",
+    "& img": {
+      position: "absolute",
+      width: "80%",
+      bottom: "24%",
+      left: "13%",
     },
   },
   animalBubble: {
