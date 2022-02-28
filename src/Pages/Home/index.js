9ec8f05c56d18dfab49bb1fa9cc312cda11d74e9
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-28 10:00:54 -0400

    feat: add hero structure

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Container, Grid, Typography } from "@mui/material";
+import styles from "./styles";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <Container sx={styles.container}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6} lg={6}>
+          <Typography variant="h3" component="h1" sx={styles.title}>
+            FindPet te ajuda a encontrar seu animal perdido!
+          </Typography>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
 
 export default Home;
---
+++ src/Pages/Home/styles.js
@@ -0,0 +1,6 @@
+const styles = {
+  container: { maxWidth: "900px" },
+  title: { fontFamily: "Pangolin" },
+};
+
+export default styles;
