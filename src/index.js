d90dddc1a9daef1328c7bf937b1d0eef662b4984
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-28 17:56:46 -0400

    style: add global styles

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "./Redux/Store/store";
 
+import "./Styles/Global/index.css";
 import App from "./App";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./Styles/Theme/theme";
