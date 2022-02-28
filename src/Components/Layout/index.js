e8c7e2bc6ed989da2eabdf0969507d22e1c9200d
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-28 18:16:35 -0400

    style(header): add brand link

--- src/Components/Layout/index.js
+++ src/Components/Layout/index.js
@@ -1,15 +1,9 @@
 import React from "react";
 import styles from "./styles";
 
-import {
-  AppBar,
-  Box,
-  Toolbar,
-  Typography,
-  Button,
-  IconButton,
-} from "@mui/material";
+import { AppBar, Box, Toolbar, Typography, Button, Link } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
+import Logo from "../Svg/Logo";
 
 export default function Layout({ children }) {
   return (
@@ -17,8 +11,22 @@ export default function Layout({ children }) {
       <Box sx={{ flexGrow: 1 }}>
         <AppBar position="static" sx={styles.appbar}>
           <Toolbar>
-            <Button color="primary" variant="contained">
-              Login
+            <Button sx={styles.logo}>
+              <Link href="/" sx={styles.logoLink}>
+                <Logo />
+              </Link>
+            </Button>
+
+            <Box sx={{ flexGrow: 1 }}></Box>
+            <Button color="primary" variant="contained" sx={styles.button}>
+              <Typography variant="p" sx={styles.buttonText}>
+                Entrar
+              </Typography>
+            </Button>
+            <Button color="secondary" variant="contained" sx={styles.button}>
+              <Typography variant="p" sx={styles.buttonText}>
+                Registrar
+              </Typography>
             </Button>
           </Toolbar>
         </AppBar>
--- src/Components/Layout/styles.js
+++ src/Components/Layout/styles.js
@@ -2,6 +2,20 @@ const styles = {
   appbar: {
     backgroundColor: "white",
   },
+  logo: {
+    width: "150px",
+  },
+  logoLink: {
+    display: "flex",
+    alignItems: "center",
+  },
+  button: {
+    margin: "0 5px",
+    padding: "8px 32px",
+  },
+  buttonText: {
+    fontSize: ".8rem",
+  },
 };
 
 export default styles;
