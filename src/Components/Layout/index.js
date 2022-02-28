658615ff992c25f80987eaf7b2f88915fde0ee81
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-28 18:21:52 -0400

    fix(header): change link to react-router link

--- src/Components/Layout/index.js
+++ src/Components/Layout/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import styles from "./styles";
 
-import { AppBar, Box, Toolbar, Typography, Button, Link } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
+import { AppBar, Box, Toolbar, Typography, Button } from "@mui/material";
+import { Link } from "react-router-dom";
 import Logo from "../Svg/Logo";
 
 export default function Layout({ children }) {
@@ -11,11 +11,11 @@ export default function Layout({ children }) {
       <Box sx={{ flexGrow: 1 }}>
         <AppBar position="static" sx={styles.appbar}>
           <Toolbar>
-            <Button sx={styles.logo}>
-              <Link href="/" sx={styles.logoLink}>
+            <Link to="/">
+              <Button sx={styles.logo}>
                 <Logo />
-              </Link>
-            </Button>
+              </Button>
+            </Link>
 
             <Box sx={{ flexGrow: 1 }}></Box>
             <Button color="primary" variant="contained" sx={styles.button}>
