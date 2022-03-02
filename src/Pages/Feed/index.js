45824c2ba41eaed0f5fbc7f72f03f0987219f432
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-03-02 00:32:33 -0400

    feat(feed): add post card

--- src/Pages/Feed/index.js
+++ src/Pages/Feed/index.js
@@ -1,13 +1,78 @@
 import React from "react";
-import styles from "styles";
+import styles from "./styles";
 
-import { Container, Typography, Button } from "@mui/material";
+import {
+  Container,
+  Typography,
+  Button,
+  Box,
+  Card,
+  CardContent,
+  IconButton,
+  CardMedia,
+  Grid,
+} from "@mui/material";
 import Layout from "../../Components/Layout";
 
 export default function Feed() {
   return (
     <Layout>
-      <Container>Heelo</Container>
+      <Container sx={styles.container}>
+        <Grid sx={styles.postContainer} container spacing={3}>
+          <Grid item>
+            <Card sx={styles.card}>
+              <CardMedia
+                component="img"
+                sx={styles.cardImage}
+                image="https://post.healthline.com/wp-content/uploads/2020/08/3180-Pug_green_grass-732x549-thumbnail-732x549.jpg"
+                alt="Live from space album cover"
+              />
+              <Box sx={styles.cardInfo}>
+                <CardContent sx={styles.cardContent}>
+                  <Typography component="div" variant="h5">
+                    Belinha
+                  </Typography>
+                  <Typography
+                    variant="p"
+                    color="text.secondary"
+                    component="div"
+                  >
+                    Espécie: Cão
+                  </Typography>
+                  <Typography
+                    variant="p"
+                    color="text.secondary"
+                    component="div"
+                  >
+                    Tutor(a): João Silva
+                  </Typography>
+                  <Typography
+                    variant="p"
+                    color="text.secondary"
+                    component="div"
+                  >
+                    Raça: SRD
+                  </Typography>
+                  <Typography
+                    variant="p"
+                    color="text.secondary"
+                    component="div"
+                  >
+                    Cidade: Cuiabá - MT
+                  </Typography>
+                  <Typography
+                    variant="p"
+                    color="text.secondary"
+                    component="div"
+                  >
+                    Data: 01/03/2022
+                  </Typography>
+                </CardContent>
+              </Box>
+            </Card>
+          </Grid>
+        </Grid>
+      </Container>
     </Layout>
   );
 }
