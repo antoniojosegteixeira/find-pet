48aae715d8501215eb696395f0ed1f2f55f804d4
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-03-02 00:56:55 -0400

    style(feed): improve card responsiveness

--- src/Pages/Feed/index.js
+++ src/Pages/Feed/index.js
@@ -14,64 +14,56 @@ import {
 } from "@mui/material";
 import Layout from "../../Components/Layout";
 
+const PostCard = () => {
+  return (
+    <Grid item xs={6} sm={4}>
+      <Card sx={styles.card}>
+        <CardMedia
+          component="img"
+          sx={styles.cardImage}
+          image="https://post.healthline.com/wp-content/uploads/2020/08/3180-Pug_green_grass-732x549-thumbnail-732x549.jpg"
+          alt="Live from space album cover"
+        />
+        <Box sx={styles.cardInfo}>
+          <CardContent sx={styles.cardContent}>
+            <Typography component="div" variant="h5">
+              Belinha
+            </Typography>
+            <Typography variant="p" color="text.secondary" component="div">
+              Espécie: Cão
+            </Typography>
+            <Typography variant="p" color="text.secondary" component="div">
+              Tutor(a): João Silva
+            </Typography>
+            <Typography variant="p" color="text.secondary" component="div">
+              Raça: SRD
+            </Typography>
+            <Typography variant="p" color="text.secondary" component="div">
+              Cidade: Cuiabá - MT
+            </Typography>
+            <Typography variant="p" color="text.secondary" component="div">
+              Data: 01/03/2022
+            </Typography>
+          </CardContent>
+        </Box>
+      </Card>
+    </Grid>
+  );
+};
+
 export default function Feed() {
   return (
     <Layout>
       <Container sx={styles.container}>
-        <Grid sx={styles.postContainer} container spacing={3}>
-          <Grid item>
-            <Card sx={styles.card}>
-              <CardMedia
-                component="img"
-                sx={styles.cardImage}
-                image="https://post.healthline.com/wp-content/uploads/2020/08/3180-Pug_green_grass-732x549-thumbnail-732x549.jpg"
-                alt="Live from space album cover"
-              />
-              <Box sx={styles.cardInfo}>
-                <CardContent sx={styles.cardContent}>
-                  <Typography component="div" variant="h5">
-                    Belinha
-                  </Typography>
-                  <Typography
-                    variant="p"
-                    color="text.secondary"
-                    component="div"
-                  >
-                    Espécie: Cão
-                  </Typography>
-                  <Typography
-                    variant="p"
-                    color="text.secondary"
-                    component="div"
-                  >
-                    Tutor(a): João Silva
-                  </Typography>
-                  <Typography
-                    variant="p"
-                    color="text.secondary"
-                    component="div"
-                  >
-                    Raça: SRD
-                  </Typography>
-                  <Typography
-                    variant="p"
-                    color="text.secondary"
-                    component="div"
-                  >
-                    Cidade: Cuiabá - MT
-                  </Typography>
-                  <Typography
-                    variant="p"
-                    color="text.secondary"
-                    component="div"
-                  >
-                    Data: 01/03/2022
-                  </Typography>
-                </CardContent>
-              </Box>
-            </Card>
+        <Box sx={styles.postWrapper}>
+          <Grid container sx={styles.postContainer} spacing={3}>
+            <PostCard />
+            <PostCard />
+            <PostCard />
+            <PostCard />
+            <PostCard />
           </Grid>
-        </Grid>
+        </Box>
       </Container>
     </Layout>
   );
--- src/Pages/Feed/styles.js
+++ src/Pages/Feed/styles.js
@@ -2,10 +2,18 @@ const styles = {
   container: {
     minHeight: "80vh",
   },
+  postWrapper: {
+    display: "flex",
+    justifyContent: "center",
+  },
+  postContainer: {
+    mt: 2,
+    display: "flex",
+    pr: 2,
+  },
   card: {
     boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
     display: "flex",
-    maxWidth: 400,
     flexDirection: "column",
   },
   cardImage: {
@@ -13,11 +21,6 @@ const styles = {
   },
   cardInfo: { display: "flex", flexDirection: "column" },
   cardContent: { flex: "1 0 auto" },
-  postContainer: {
-    mt: 2,
-    display: "flex",
-    justifyContent: "center",
-  },
 };
 
 export default styles;
