f0fdf23beddb679bde5700d6149bb0dcb04bfd9f
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-03-01 21:54:44 -0400

    feat: add feed page

---
+++ src/Pages/Feed/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "styles";
+
+import { Container, Typography, Button } from "@mui/material";
+import Layout from "../../Components/Layout";
+
+export default function Feed() {
+  return (
+    <Layout>
+      <Container>Heelo</Container>
+    </Layout>
+  );
+}
