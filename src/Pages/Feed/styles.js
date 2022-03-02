33b15108e35d3d09a24cc2388a2d3c45a132db85
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-03-02 00:32:44 -0400

    style(feed): style post card

--- src/Pages/Feed/styles.js
+++ src/Pages/Feed/styles.js
@@ -1,3 +1,23 @@
-const styles = {};
+const styles = {
+  container: {
+    minHeight: "80vh",
+  },
+  card: {
+    boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+    display: "flex",
+    maxWidth: 400,
+    flexDirection: "column",
+  },
+  cardImage: {
+    width: "100%",
+  },
+  cardInfo: { display: "flex", flexDirection: "column" },
+  cardContent: { flex: "1 0 auto" },
+  postContainer: {
+    mt: 2,
+    display: "flex",
+    justifyContent: "center",
+  },
+};
 
 export default styles;
